e61dea7c5f4b63a4bdedf1b7fcd3a64df4fada8f
vinimaffra03 <viniciushmdem15@gmail.com>
2025-04-19 20:19:06 -0300

    condicional para body

--- alura-books-server/controladores/livroscontrol.js
+++ alura-books-server/controladores/livroscontrol.js
@@ -20,8 +20,14 @@ function getLivros(req, res) {
 function getLivro(req, res) {
   try {
     const id = req.params.id;
-    const livro = getLivroPorId(id);
-    res.send(livro);
+
+    if (id && Number(id)) {
+      const livro = getLivroPorId(id);
+      res.send(livro);
+    } else {
+      res.status(422);
+      res.send("ID invalido, ID deve ser um numero");
+    }
   } catch (error) {
     res.status(500);
     res.send(error.message);
@@ -31,9 +37,15 @@ function getLivro(req, res) {
 function postLivro(req, res) {
   try {
     const livroNovo = req.body;
-    insereLivro(livroNovo);
 
-    res.send("Livro adicionado com sucesso");
+    if (req.body.nome) {
+      insereLivro(livroNovo);
+      res.status(201);
+      res.send("Livro adicionado com sucesso");
+    } else {
+      res.status(422);
+      res.send("Nome do livro é obrigatório");
+    }
   } catch (error) {
     res.status(500);
     res.send(error.message);
@@ -56,8 +68,14 @@ function patchLivro(req, res) {
 function deleteLivro(req, res) {
   try {
     const id = Number(req.params.id);
-    deletaLivroPorId(id);
-    res.send("livro deletado com sucesso");
+
+    if (id && Number(id)) {
+      deletaLivroPorId(id);
+      res.send("livro deletado com sucesso");
+    } else {
+      res.status(422);
+      res.send("ID invalido, ID deve ser um numero");
+    }
   } catch (error) {
     res.status(500);
     res.send(error.message);
